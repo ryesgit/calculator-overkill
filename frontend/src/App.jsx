931ebafc3f001c2e646a221796e66498cbdaf2e7
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-05-07 17:34:13 +0800

    Create Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Heading from './components/Heading.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <main className=' bg-slate-900 h-screen w-screen m-0 p-0'>
+        <Heading></Heading>
       </main>
     </>
   )
---
+++ frontend/src/components/Heading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <div className=' bg-black'>
+        <h1 className=' text-3xl font-bold text-white shadow-2xl shadow-yellow-500 px-4 py-2'>Calculator <span className=' text-red-700'>Overkill!</span></h1>
+    </div>
+  )
+}
+
+export default Heading
