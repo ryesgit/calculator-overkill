38f5c1635f633e2d5a09d287edb354ea014b96d7
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-05-07 21:34:13 +0800

    Enable calculate API call

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from calculator import calculator
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/calculate', methods=["POST"])
+def calculate():
+    req_body = request.json
+    num1 = req_body.get('num1')
+    num2 = req_body.get('num2')
+    mode = req_body.get('mode')
+    result = calculator(num1, num2, mode)
+    print(result)
+    return jsonify(result), 200
 
 if __name__ == '__main__':
     app.run()
