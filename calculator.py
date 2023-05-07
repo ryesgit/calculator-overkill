e74ca5da32d634e79a50006104fb2462b0685745
ryesgit <128949576+ryesgit@users.noreply.github.com>
2023-05-07 15:41:06 +0800

    Initial commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+'''
+filename: calculator.py
+
+This module acquires two numbers, and then performs mathematical operations to them
+according to the mode (addition, subtraction, multiplication, and division) also will provide. 
+
+1. Ask for two numbers and the mode of operation
+2. Perform mathematical operations.
+    - If addition, then add numbers
+    - If subtraction, then subtract numbers
+    - If multiplication, then multiply numbers
+    - If division, then divide numbers.
+3. Catch program errors! For example, the user must not be able to divide a number by zero.
+
+'''
